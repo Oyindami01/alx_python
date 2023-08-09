7f1862418e5ac717f2bd59fd31ddaa4b79cc72e8
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-08-09 05:02:31 +0100

    Learning python

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3= "Hol"
+word_last_2= "on"
+middle_word= "berton"
+print("Holberton: {}".format[3:])
+print("Holberton: {}".format[-2:])
+print("Holberton: {}".format[1: -1])
