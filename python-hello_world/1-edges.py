cdb6f8921de26a159582869ad9d71de80487d32b
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-08-09 10:08:56 +0100

    learning python

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3= word[:3] 
-print("Holberton:{}".format(word_first_3))
+print("First 3 leters: {}".format(word_first_3))
 word_last_2= word[-2:]
-print("Holberton:{}".format(word_last_2))
+print("Last 2 letters: {}".format(word_last_2))
 middle_word= word[1: -1]
-print("Holberton:{}".format[middle_word])
+print("Middle word: {}".format[middle_word])
