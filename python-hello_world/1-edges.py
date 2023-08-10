8d3f77d4561c54077a7680644d723eee7c003a66
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-08-10 06:16:57 +0100

    learning python

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -5,4 +5,4 @@ print("First 3 letters: {}".format(word_first_3))
 word_last_2 = word[-2:]
 print("Last 2 letters: {}".format(word_last_2))
 middle_word = word[1:-1]
-print("middle word: {}".format(middle_word))
+print("Middle word: {}".format(middle_word))
