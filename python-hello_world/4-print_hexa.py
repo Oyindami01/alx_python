8d394a895cb3c4942a7897738a83cb3b61b5389e
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-08-10 15:29:45 +0100

    learning python

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for number in range(99):
-    print("Decimal:", number, "Hexadecimal:", hex(number))
+for number in range(0, 99):
+    print("{} =0x{:x}" .format(number, number))
