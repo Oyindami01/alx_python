5d30429cac9c3832e1c7c803f4bf22e050ddd315
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-07 20:38:14 +0100

    removed the blank line

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,36 +1,9 @@
-"""
-Script to list all cities from the hbtn_0e_4_usa database.
-
-Usage:
-    python script.py <username> <password> <database>
-
-This script connects to a MySQL server running on localhost at port 3306.
-It retrieves a sorted list of cities from the specified database
-and displays them along with their state names.
-
-Arguments:
-    <username>: MySQL username.
-    <password>: MySQL password.
-    <database>: Database name.
-"""
-
 import MySQLdb
 import sys
 
-
-def list_cities(username, password, database):
-    """
-    Fetches and lists all cities from the hbtn_0e_4_usa database.
-
-    Args:
-        <username>: MySQL username.
-        <password>: MySQL password.
-        <database>: Database name.
-    Returns:
-        None
-    """
+def list_cities_by_state(username, password, database, state_name):
     try:
-        # Connect to the server using the context manager
+        # Connect to the MySQL server using the context manager
         with MySQLdb.connect(
             host='localhost',
             user=username,
@@ -38,30 +11,31 @@ def list_cities(username, password, database):
             db=database,
             port=3306
         ) as connection:
-            # Create a cursor that aids interaction with the database
+            
             cursor = connection.cursor()
-            # Execute the SQL query to retrieve cities along with state names
+            
+            # Use a single execute() statement to fetch the results
             query = (
-                "SELECT cities.id, cities.name, states.name "
+                "SELECT GROUP_CONCAT(cities.name ORDER BY cities.id ASC SEPARATOR ', ') "
                 "FROM cities "
                 "JOIN states ON cities.state_id = states.id "
-                "ORDER BY cities.id ASC"
+                "WHERE states.name = %s"
             )
-            cursor.execute(query)
-            # Fetch all the rows from the result
-            rows = cursor.fetchall()
-
-            # Print results
-            for row in rows:
-                print(row)
+            cursor.execute(query, (state_name,))
+            
+            # Fetch and print the result
+            result = cursor.fetchone()[0]
+            if result:
+                print(result)
     except MySQLdb.Error as e:
         print("MySQL Error:", e)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database> <state_name>")
     else:
         username = sys.argv[1]
         password = sys.argv[2]
         database = sys.argv[3]
-        list_cities(username, password, database)
+        state_name = sys.argv[4]
+        list_cities_by_state(username, password, database, state_name)
