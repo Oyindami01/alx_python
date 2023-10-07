d74b84b5c0db758a23c114b9e4b44c705e7e64ec
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-07 20:43:04 +0100

    rearranged the file

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ def list_cities_by_state(username, password, database, state_name):
             
             # Use a single execute() statement to fetch the results
             query = (
-                "SELECT GROUP_CONCAT(cities.name ORDER BY cities.id ASC SEPARATOR ', ') "
+                "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') "
                 "FROM cities "
                 "JOIN states ON cities.state_id = states.id "
                 "WHERE states.name = %s"
@@ -24,9 +24,10 @@ def list_cities_by_state(username, password, database, state_name):
             cursor.execute(query, (state_name,))
             
             # Fetch and print the result
-            result = cursor.fetchone()[0]
-            if result:
-                print(result)
+            result = cursor.fetchone()
+            
+            if result[0]:
+                print(result[0])
     except MySQLdb.Error as e:
         print("MySQL Error:", e)
 
