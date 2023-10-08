9ef6ba872b802b96d3107134a0f8b73878dba69b
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-08 19:27:37 +0100

    updated the 1-export file

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,49 +1,46 @@
-import requests
 import csv
+import requests
+
 import sys
 
-def export_to_csv(user_id):
+def export_tasks_to_csv(user_id):
+    # Define the URL of the API
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+
     try:
-        # Fetch user data
-        user_url = 'https://jsonplaceholder.typicode.com/users/' + str(user_id)
-        response = requests.get(user_url)
-        user_data = response.json()
+        # Send a GET request to the API
+        response = requests.get(url)
 
-        # Fetch tasks for the user
-        tasks_url = 'https://jsonplaceholder.typicode.com/todos?userId=' + str(user_id)
-        response = requests.get(tasks_url)
-        tasks = response.json()
+        # Check if the request was successful
+        if response.status_code == 200:
+            tasks = response.json()
 
-        # Create a CSV file named USER_ID.csv
-        csv_filename = str(user_id) + '.csv'
+            # Define the CSV filename based on the USER_ID
+            csv_filename = f"{user_id}.csv"
 
-        # Open the CSV file for writing
-        with open(csv_filename, mode='w', newline='') as csv_file:
-            fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            
-            # Write the CSV header
-            writer.writeheader()
+            # Write the tasks to a CSV file
+            with open(csv_filename, mode='w', newline='') as csv_file:
+                fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                writer.writeheader()
 
-            # Write task data to the CSV file
-            for task in tasks:
-                writer.writerow({
-                    "USER_ID": user_id,
-                    "USERNAME": user_data['username'],
-                    "TASK_COMPLETED_STATUS": str(task['completed']),
-                    "TASK_TITLE": task['title']
-                })
+                for task in tasks:
+                    writer.writerow({
+                        "USER_ID": user_id,
+                        "USERNAME": user["name"],  # Replace with the actual username retrieval logic
+                        "TASK_COMPLETED_STATUS": str(task["completed"]),
+                        "TASK_TITLE": task["title"]
+                    })
 
-        print(f'Data exported to {csv_filename}')
-    
+            print(f"Tasks exported to {csv_filename}")
+        else:
+            print("Failed to retrieve tasks. API request failed.")
     except requests.exceptions.RequestException as e:
-        print("Error fetching data:", e)
-    except Exception as e:
-        print("An error occurred:", e)
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <USER_ID>")
     else:
         user_id = int(sys.argv[1])
-        export_to_csv(user_id)
+        export_tasks_to_csv(user_id)
