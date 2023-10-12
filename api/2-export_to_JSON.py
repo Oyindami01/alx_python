1c0ac54851ebe2ac8f9049c9a2ff73960a263760
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-12 17:48:23 +0100

    corrected the Code

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,54 +7,28 @@ import json
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python 2-export_to_JSON.py <USER_ID>")
-    sys.exit(1)
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
+    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
+    data = api_request.text
+    pjson = json.loads(data)
+    data1 = api_request1.text
+    pjson1 = json.loads(data1)
 
-user_id = sys.argv[1]
+    name_info = pjson['username']
 
-# Define the URL for the user's tasks
-url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    filename = "{}.json".format(employee_id)
 
-# Send a GET request to the API to retrieve the user's tasks
-response = requests.get(url)
-
-# Check if the request was successful
-if response.status_code != 200:
-    print(f"Failed to retrieve data for USER_ID {user_id}")
-    sys.exit(1)
-
-# Parse the JSON response
-tasks = response.json()
-
-# Check if the retrieved data is a list of tasks
-if not isinstance(tasks, list):
-    print(f"Data retrieved for USER_ID {user_id} is not a list of tasks")
-    sys.exit(1)
-
-# Retrieve the user's information from the API
-user_info_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-user_info_response = requests.get(user_info_url)
-user_info = user_info_response.json()
-
-if 'username' not in user_info:
-    print(f"Failed to retrieve username for USER_ID {user_id}")
-    sys.exit(1)
-
-# Create a dictionary to store the tasks
-task_dict = {user_id: []}
-
-# Populate the dictionary with task information
-for task in tasks:
-    task_info = {
-        "task": task["title"],
-        "completed": task["completed"],
-        "username": user_info["username"]
+    # Create the dictionary structure
+    result = {
+        employee_id: [{
+            "task": item["title"],
+            "completed": item["completed"],
+            "username": name_info
+        } for item in pjson1]
     }
-    task_dict[user_id].append(task_info)
+    # export data to json file
 
-# Write the task dictionary to a JSON file
-with open(f"{user_id}.json", "w") as json_file:
-    json.dump(task_dict, json_file)
-
-print(f"Data exported to {user_id}.json")
+    with open(filename, "w") as outfile:
+        json.dump(result, outfile)
