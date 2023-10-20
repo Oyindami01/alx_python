361487dc61c263ccab60db8b227493a2bb882815
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-20 04:02:42 +0100

    corrected the code to print out negative

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python3
-import random
-
-number = random.randint(-10000, 10000)
+number = -98
 last_digit = number % 10
 
 print("Last digit of", number, "is", last_digit, end=" ")
@@ -12,4 +9,3 @@ elif last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-    
