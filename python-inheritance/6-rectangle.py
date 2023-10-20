aaf42edbdab8a2bb78e900509e2df6c7221bab10
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-20 08:04:29 +0100

    corrected the code

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -7,6 +7,11 @@ class BaseGeometry:
 
 class Rectangle(BaseGeometry):
     def __init__(self, width=0, height=0):
+        if width <= 0:
+            raise ValueError("width must be greater than 0")
+        if height <= 0:
+            raise ValueError("height must be greater than 0")
+
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
@@ -15,8 +20,6 @@ class Rectangle(BaseGeometry):
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name)
 
 # Test cases
 if __name__ == "__main__":
