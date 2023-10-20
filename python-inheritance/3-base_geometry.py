f68e0cf32e5e4321cb951ef972c728fb757b02e1
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-20 07:55:04 +0100

    corrected my code

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 
+"""
+This module defines a function to check if an object is exactly an instance of a specified class.
+
+Function:
+    is_same_class(obj, a_class): Checks if an object is exactly an instance of the specified class.
+"""
+
 class BaseGeometry:
+    """
+    This is an empty class representing our base geometry.
+
+    It can be used as a base class to create other geometry-related classes.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
     def __dir__(self):
-        # Get the list of attributes from the parent class (object class)
-        attributes = super().__dir()
+        """
+        Get the list of attributes from the parent class (object class).
+        """
+        attributes = super().__dir__()
 
-        # Exclude '__init_subclass__' from the list of attributes
-        attributes = [attr for attr in attributes if attr != '__init_subclass__']
-
-        return attributes
-
-
-geometry = BaseGeometry()
-print(dir(geometry))
+        # Exclude __init_subclass__ from the list of attributes.
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
