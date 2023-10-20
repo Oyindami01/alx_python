81cf7fcb96d9978f0d34abbe6bd5b4f9d7ed3dc5
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-20 07:33:56 +0100

    corrected the code errors

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,12 +3,13 @@ import sys
 
 def print_arguments():
     num_arguments = len(sys.argv) - 1
-    plural = 's' if num_arguments != 1 else ''
-    print("{num_arguments} argument{plural}:".format(num_arguments=num_arguments, plural=plural))
+    if num_arguments == 0:
+        print("0 arguments.")
+    else:
+        print("{num_arguments} argument{plural}:".format(num_arguments=num_arguments, plural='s' if num_arguments != 1 else ''))
     
-    if num_arguments > 0:
-        for i, arg in enumerate(sys.argv[1:], start=1):
-            print("{i}: {arg}".format(i=i, arg=arg))
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{i}: {arg}".format(i=i, arg=arg))
 
 if __name__ == "__main__":
     print_arguments()
