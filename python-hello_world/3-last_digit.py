b1ff29f6e4ad0f462211258d48b396564b951c83
Oyindami01 <131830038+Oyindami01@users.noreply.github.com>
2023-10-20 06:13:53 +0100

    corrected the code to print out negative value

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import random
 
 number = random.randint(-10000, 10000)
@@ -15,3 +15,4 @@ elif last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
+    
