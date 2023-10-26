5b59b0ecb8cf9b6ee2483d14cd4f36e8c1054f11
Olapeju <131830038+Oyindami01@users.noreply.github.com>
2023-10-26 08:22:35 +0100

    updated the file

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -18,7 +18,6 @@ def user_info(user_id):
 
     data = {str(user_id): []}
     for task in tasks:
-        
         data[str(user_id)].append({
             "task": task['title'],
             "completed": task['completed'],
@@ -29,18 +28,6 @@ def user_info(user_id):
         json.dump(data, jsonfile, indent=2)
 
     print(f'Tasks have been exported to {json_file}')
-
-    # Check if the correct format is met
-    with open(json_file, 'r') as jsonfile:
-        loaded_data = json.load(jsonfile)
-        if str(user_id) in loaded_data and isinstance(loaded_data[str(user_id)], list):
-            print("USER_ID's value type is a list of dicts: OK")
-
-    # Check if all tasks are found
-    if len(data[str(user_id)]) == len(tasks):
-        print("All tasks found: OK")
-    else:
-        print(f"Number of tasks missing: {len(tasks) - len(data[str(user_id)])}")
 
 if len(sys.argv) < 2:
     print('Please provide the user ID as an argument.')
