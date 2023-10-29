f4cba3fbdd88f78309f389ee60da4ba104666112
Olapeju <131830038+Oyindami01@users.noreply.github.com>
2023-10-29 12:22:27 +0100

    corrected the cxode

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -7,7 +7,6 @@ Function:
     is_same_class(obj, a_class): Checks if an object is exactly an instance of the specified class.
 """
 class BaseGeometry:
-    pass
     """
     This is an empty class representing our base geometry.
 
