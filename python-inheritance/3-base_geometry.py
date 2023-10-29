42ffd674282a83ac5d7e1ee5f505b82e8611f04d
Olapeju <131830038+Oyindami01@users.noreply.github.com>
2023-10-29 12:12:10 +0100

    addedc print

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -25,6 +25,13 @@ class BaseGeometry:
         """
         attributes = super().__dir__()
 
-        """ Exclude __init_subclass__ from the list of attributes
-        """
-        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+        # Debugging: Print the attributes before exclusion.
+        print("Before exclusion:", attributes)
+
+        # Exclude '__init_subclass__' from the list of attributes.
+        attributes = [attr for attr in attributes if attr != '__init_subclass__']
+
+        # Debugging: Print the attributes after exclusion.
+        print("After exclusion:", attributes)
+
+        return attributes
